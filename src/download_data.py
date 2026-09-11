dedcea4c0f626d5792e13e74656f6a9c32684322
LiangC <liangcamilo2006@gmail.com>
2026-09-11 10:32:16 -0500

    fix: fixed problem with dataset download

--- src/download_data.py
+++ src/download_data.py
@@ -4,7 +4,17 @@ import kagglehub;
 
 ROOT_DIR = Path(__file__).resolve().parent.parent;
 DATA_DIR = ROOT_DIR / "data" / "raw";
+GITKEEP = DATA_DIR / ".gitkeep"
 
-path = kagglehub.dataset_download("maharshipandya/-spotify-tracks-dataset", output_dir=str(DATA_DIR));
+if GITKEEP.exists():
+    GITKEEP.unlink();
+
+path = kagglehub.dataset_download(
+        "maharshipandya/-spotify-tracks-dataset", 
+        output_dir=str(DATA_DIR), 
+        force_download=True
+    );
+
+GITKEEP.touch();
 
 print(path);
